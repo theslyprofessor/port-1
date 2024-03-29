20a71c7344aa86aa020e3db8bcb50ed8c581cdfb
theslyprofessor <ntiruviluamala@gmail.com>
2024-03-28 22:40:50 -0700

    Revert "rewrite port.js to use ES6 class instead of prototype, modified division.js to invoke pd class using 'new' argument"
    This reverts commit 16ac3a86bd36efa7a8f373475521fd646d58b325.

--- examples/testing/division.js
+++ examples/testing/division.js
@@ -53,7 +53,7 @@ function run(){
 	pd.write(tests[i].input.join(';\n') + ';\n');
 }
 
-var pd = new port({
+var pd = port({
 	'read': 8105, // [netsend]
 	'write': 8106, // [netreceive]
 	'flags': {
